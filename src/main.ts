beb108f937c2b1c8784881274ca18eabf8f8eeef
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-29 00:47:03 -0600

    [ADD] main file

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './infrastructure/routes/userRoutes';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI as string)
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Could not connect to MongoDB Atlas', err));
+
+app.use('/api/users', userRoutes);
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
